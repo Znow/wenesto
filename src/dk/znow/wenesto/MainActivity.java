0d7fb24a88967df53e42e288a8679504a07250a1
Mikkel Møller Jensen <Mikkel2833@gmail.com>
2014-02-10 10:16:26 +0100

    Initial commit of Swiping tab views.
    First commit for the different swiping tab views.

--- src/dk/znow/wenesto/MainActivity.java
+++ src/dk/znow/wenesto/MainActivity.java
@@ -1,15 +1,83 @@
 package dk.znow.wenesto;
 
+import dk.znow.wenesto.adapter.TabsPagerAdapter;
 import android.os.Bundle;
+import android.app.ActionBar;
+import android.app.ActionBar.Tab;
+import android.app.ActionBar.TabListener;
 import android.app.Activity;
+import android.app.FragmentTransaction;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.ViewPager;
 import android.view.Menu;
 
-public class MainActivity extends Activity {
-
+public class MainActivity extends FragmentActivity implements TabListener
+{
+	
+	//Tab titles
+	private String[] tabs = {"Weather","News","Stocks"};
+	private ViewPager viewPager;
+	private TabsPagerAdapter tabsAdapter;
+	private ActionBar actionBar;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		viewPager = (ViewPager)findViewById(R.id.pager);
+		actionBar = getActionBar();
+		tabsAdapter = new TabsPagerAdapter(getSupportFragmentManager());
+		
+		viewPager.setAdapter(tabsAdapter);
+		//actionBar.setHomeButtonEnabled = false;
+		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+		
+		//Add tabs
+		
+		for(String s : tabs)
+		{
+			actionBar.addTab(actionBar.newTab().setText(s).setTabListener(this));
+		}
+	
+
+    viewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+    	 
+        @Override
+        public void onPageSelected(int position) {
+            // on changing the page
+            // make respected tab selected
+            actionBar.setSelectedNavigationItem(position);
+        }
+
+        @Override
+        public void onPageScrolled(int arg0, float arg1, int arg2) {
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int arg0) {
+        }
+    });
 	}
+	
+	@Override
+	public void onTabReselected(Tab arg0, FragmentTransaction arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onTabSelected(Tab tab, FragmentTransaction arg1) 
+	{
+		//On tab selected, show respected fragment view.
+		viewPager.setCurrentItem(tab.getPosition());
+	}
+
+	@Override
+	public void onTabUnselected(Tab arg0, FragmentTransaction arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+	
 
 }
