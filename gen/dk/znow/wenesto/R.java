15b998380225983be3873e2ded56bf5245f762d8
Daniel Gottschalck <znowm4n@gmail.com>
2014-02-10 13:23:55 +0100

    dsa

--- gen/dk/znow/wenesto/R.java
+++ gen/dk/znow/wenesto/R.java
@@ -24,31 +24,36 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080012;
-        public static final int border2=0x7f08000b;
-        public static final int conditionText=0x7f080008;
-        public static final int dateText=0x7f080005;
-        public static final int day1=0x7f08000e;
-        public static final int day2=0x7f08000f;
-        public static final int day3=0x7f080010;
-        public static final int day4=0x7f080011;
-        public static final int footer_border=0x7f080004;
-        public static final int forecastBorder=0x7f08000d;
-        public static final int forecast_title=0x7f08000c;
-        public static final int humidityText=0x7f080009;
-        public static final int icon=0x7f080006;
+        public static final int action_settings=0x7f080016;
+        public static final int border2=0x7f08000f;
+        public static final int conditionText=0x7f08000c;
+        public static final int dateText=0x7f080009;
+        public static final int day1=0x7f080012;
+        public static final int day2=0x7f080013;
+        public static final int day3=0x7f080014;
+        public static final int day4=0x7f080015;
+        public static final int footer_border=0x7f080008;
+        public static final int forecastBorder=0x7f080011;
+        public static final int forecast_title=0x7f080010;
+        public static final int fragment_layout=0x7f080002;
+        public static final int humidityText=0x7f08000d;
+        public static final int icon=0x7f08000a;
+        public static final int itemTitle=0x7f080001;
+        public static final int listView=0x7f080003;
         public static final int pager=0x7f080000;
-        public static final int scrollView1″=0x7f080003;
-        public static final int tempText=0x7f080007;
-        public static final int weather_border=0x7f080002;
-        public static final int weather_title=0x7f080001;
-        public static final int windText=0x7f08000a;
+        public static final int progressBar=0x7f080004;
+        public static final int scrollView1″=0x7f080007;
+        public static final int tempText=0x7f08000b;
+        public static final int weather_border=0x7f080006;
+        public static final int weather_title=0x7f080005;
+        public static final int windText=0x7f08000e;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
-        public static final int newsfragment=0x7f030001;
-        public static final int stocksfragment=0x7f030002;
-        public static final int weatherfragment=0x7f030003;
+        public static final int news_item=0x7f030001;
+        public static final int newsfragment=0x7f030002;
+        public static final int stocksfragment=0x7f030003;
+        public static final int weatherfragment=0x7f030004;
     }
     public static final class menu {
         public static final int main=0x7f070000;
