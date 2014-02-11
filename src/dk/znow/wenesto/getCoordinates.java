57765b3e43bc199aea2aa4495809d00051364a70
Mikkel Møller Jensen <Mikkel2833@gmail.com>
2014-02-11 15:05:17 +0100

    update

--- src/dk/znow/wenesto/getCoordinates.java
+++ src/dk/znow/wenesto/getCoordinates.java
@@ -42,55 +42,62 @@ public class getCoordinates extends Service implements LocationListener{
 	    }
 	    public Location getLocation() {
 	        try {
-	            locationManager = (LocationManager) mContext
-	                    .getSystemService(Context.LOCATION_SERVICE);
+	            locationManager = (LocationManager) mContext.getSystemService(Context.LOCATION_SERVICE);
 	 
 	            // getting GPS status
-	            isGPSEnabled = locationManager
-	                    .isProviderEnabled(LocationManager.GPS_PROVIDER);
-	 
+	            isGPSEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+	   
 	            // getting network status
-	            isNetworkEnabled = locationManager
-	                    .isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+	            isNetworkEnabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
 	 
 	            if (!isGPSEnabled && !isNetworkEnabled) {
 	                // no network provider is enabled
 	            } else {
 	                this.canGetLocation = true;
 	                // First get location from Network Provider
-	                if (isNetworkEnabled) {
-	                    locationManager.requestLocationUpdates(
-	                            LocationManager.NETWORK_PROVIDER,
-	                            MIN_TIME_BW_UPDATES,
-	                            MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
-	                    Log.d("Network", "Network");
-	                    if (locationManager != null) {
-	                        location = locationManager
-	                                .getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
-	                        if (location != null) {
-	                            latitude = location.getLatitude();
-	                            longitude = location.getLongitude();
-	                        }
-	                    }
+	                if (isNetworkEnabled) 
+	                {
+//	                    locationManager.requestLocationUpdates(
+//	                            LocationManager.NETWORK_PROVIDER,
+//	                            MIN_TIME_BW_UPDATES,
+//	                            MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
+//	                    Log.d("Network", "Network");
+//	                    if (locationManager != null) {
+//	                        location = locationManager
+//	                                .getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+//	                        if (location != null) {
+//	                            latitude = location.getLatitude();
+//	                            longitude = location.getLongitude();
+//	                        }
+//	                    }
 	                }
 	                // if GPS Enabled get lat/long using GPS Services
-	                if (isGPSEnabled) {
-	                    if (location == null) {
+	                if (isGPSEnabled) 
+	                {
+	                    if (location == null) 
+	                    {
 	                        locationManager.requestLocationUpdates(
 	                                LocationManager.GPS_PROVIDER,
 	                                MIN_TIME_BW_UPDATES,
 	                                MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
 	                        Log.d("GPS Enabled", "GPS Enabled");
-	                        if (locationManager != null) {
-	                            location = locationManager
-	                                    .getLastKnownLocation(LocationManager.GPS_PROVIDER);
-	                            if (location != null) {
+	                        if (locationManager != null) 
+	                        {
+	                            location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+	                            if (location != null) 
+	                            {
 	                                latitude = location.getLatitude();
 	                                longitude = location.getLongitude();
+	                                Log.d("Latitude",latitude+"");
+	                                Log.d("Longitude",longitude+"");
 	                            }
+	                            else
+	                            	Log.d("SUT MIN","DILLER");
 	                        }
 	                    }
 	                }
+	                else
+	                	Log.d("GPS FAILED", "GPS FAILED");
 	            }
 	 
 	        } catch (Exception e) {
@@ -105,7 +112,7 @@ public class getCoordinates extends Service implements LocationListener{
 		   String str ="";
 	    	if (location != null)
 	    	{
-	    		str = longitude+","+latitude;
+	    		str = latitude+","+longitude;
 	    	}
 	    	return str;
 	    }
